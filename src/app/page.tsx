d09fdadaa62a98149550a566e30fc74e64a80c47
João Kalebe Carneiro Reis Costa <joaokalebecarneiro@gmail.com>
2026-09-16 19:39:09 -0300

    butao de pausar mas sem função adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,12 @@ import { useState } from "react"
 
 export default function Layout(){
   const [contagemAtual,setContagemAtual] = useState(0)
-  const Ativação = useState(true)
+  const [AtivaçãoTimer,setAtivaçãoTimer] = useState(false)
+  const [textoDoPause, setTextoDoPause] = useState('Iniciar')
 
   function subirContagem(){
-    
-
     setContagemAtual(contagemAtual + 1)
-
   }
-
   function descerContagem(){
     if(contagemAtual === 0){
         return
@@ -21,8 +18,22 @@ export default function Layout(){
     setContagemAtual(contagemAtual - 1)
   }
   function zerarContagem(){
+    setAtivaçãoTimer(false)
     setContagemAtual(0)
   }
+  function pausarDespausar(){
+    const interruptor = !AtivaçãoTimer
+    setAtivaçãoTimer(interruptor)
+    if(AtivaçãoTimer === false){
+      setTextoDoPause('Iniciar')
+    }
+    else{
+      setTextoDoPause('Pausar')
+    }
+  }
+  
+
+
   return(
     <>
     <div className='text-white border-2 border-white-900'>
@@ -31,6 +42,7 @@ export default function Layout(){
       <button onClick={subirContagem} className="bg-green-500 text-white p-4">Subir</button>
       <button onClick={descerContagem} className='bg-red-500 text-white p-4'>Descer</button>
       <button onClick={zerarContagem} className="bg-gray-700">Zerar</button>
+      <button onClick={pausarDespausar}>{textoDoPause}</button>
     </div>
     </>
   )
